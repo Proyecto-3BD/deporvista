83928640eb869fa6e992df7442103b9718dc1b3c
veoDoble <gustavoguerrerom@gmail.com>
2022-10-14 16:47:34 -0300

    fix

--- backoffice/vistas/templates/head.php
+++ backoffice/vistas/templates/head.php
@@ -49,7 +49,7 @@
           <?php if(isset($_SESSION['autenticado'])): ?>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="cerrarSesion">Salir</a>
+                    <a class="nav-link" href="/cerrarSesion">Salir</a>
                 </li>
             
           <?php endif; ?>
