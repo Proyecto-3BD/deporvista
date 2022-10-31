590d0174b9f11eee6485e150032116af64b8affb
francomacedo96 <francomacedo@outlook.com>
2022-10-31 16:51:57 -0300

    rezar para que no se rompa

--- frontend/public-html/script/infoDeportista.js
+++ frontend/public-html/script/infoDeportista.js
@@ -13,3 +13,60 @@ let url = "http://localhost:8084/deportistas";
                 }
                 document.getElementById('datadeportistas').innerHTML = body;
 }
+
+function fetchEvento() {
+
+    let url = "http://localhost:8084/eventos";
+    fetch(url)
+        .then(response => response.json())
+        .then(dataevento => mostrarData(dataevento))
+        .catch(error => console.log(error))
+
+
+    const mostrarData = (dataevento) => {
+        console.log(dataevento)
+        let body = ''
+        for (let i = 0; i < dataevento.length; i++) {
+            body += `<tr><td>${dataevento[i].idEvento}</td><td>${dataevento[i].fechaHora}</td><td>${dataevento[i].resultado}</td><td>${dataevento[i].infracciones}</td><td>${dataevento[i].ubicacion}</td></tr>`
+        }
+        document.getElementById('dataevento').innerHTML = body;
+    }
+}
+
+function fetchEventoFinalizado() {
+
+    let url = "http://localhost:8084/eventos";
+    fetch(url)
+        .then(response => response.json())
+        .then(dataevento => mostrarData(dataevento))
+        .catch(error => console.log(error))
+
+
+    const mostrarData = (dataevento) => {
+        console.log(dataevento)
+        let body = ''
+        for (let i = 0; i < dataevento.length; i++) {
+            body += `<tr><td>${dataevento[i].fechaHora}</td><td>${dataevento[i].resultado}</td><td>${dataevento[i].resultado}</td><td>${dataevento[i].infracciones}</td><td>${dataevento[i].ubicacion}</td></tr>`
+        }
+        document.getElementById('dataeventoFin').innerHTML = body;
+    }
+}
+
+function fetchEventoProximo() {
+
+    let url = "http://localhost:8084/eventos";
+    fetch(url)
+        .then(response => response.json())
+        .then(dataevento => mostrarData(dataevento))
+        .catch(error => console.log(error))
+
+
+    const mostrarData = (dataevento) => {
+        console.log(dataevento)
+        let body = ''
+        for (let i = 0; i < dataevento.length; i++) {
+            body += `<tr><td>${dataevento[i].fechaHora}</td><td>${dataevento[i].infracciones}</td><td>${dataevento[i].ubicacion}</td></tr>`
+        }
+        document.getElementById('dataeventoProx').innerHTML = body;
+    }
+}
