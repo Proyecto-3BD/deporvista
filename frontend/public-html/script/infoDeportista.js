e8fbe0e83a0c64a25958a0be11e20b116701653f
francomacedo96 <francomacedo@outlook.com>
2022-10-31 12:31:24 -0300

    error

--- frontend/public-html/script/infoDeportista.js
+++ frontend/public-html/script/infoDeportista.js
@@ -8,7 +8,7 @@ let url = "http://localhost:8084/deportistas";
             const mostrarData = (datadeportistas) => {
                 console.log(datadeportistas)
                 let body = ''
-                for (let i = 0; i < data.length; i++) {
+                for (let i = 0; i < datadeportistas.length; i++) {
                     body += `<tr><td>${datadeportistas[i].idDeportista /*match llave de json*/}</td><td>${datadeportistas[i].nombre}</td><td>${datadeportistas[i].apellidos}</td><td>${datadeportistas[i].rol}</td><td>${datadeportistas[i].pais}</td></tr>`
                 }
                 document.getElementById('datadeportistas').innerHTML = body;
