85232b590e212891b84f19dab981cdf710b4d69f
francomacedo96 <francomacedo@outlook.com>
2022-11-03 21:46:40 -0300

    prueba grande de js

--- frontend/public-html/script/infoDeportista.js
+++ frontend/public-html/script/infoDeportista.js
@@ -1,10 +1,14 @@
-let url = "http://localhost:8084/resultados";
-fetch(url)
-    .then(response => response.json())
-    .then(dataevento => mostrarData(dataevento))
-    .catch(error => console.log(error))
-
 let date = new Date();
+
+let fecha =
+    date.getFullYear() + "-" +
+    ("00" + (date.getMonth() + 1)).slice(-2) + "-" +
+    ("00" + date.getDate()).slice(-2) + " " +
+
+    ("00" + date.getHours()).slice(-2) + ":" +
+    ("00" + date.getMinutes()).slice(-2) + ":" +
+    ("00" + date.getSeconds()).slice(-2);
+console.log(fecha);
 
 let principioDia =
     date.getFullYear() + "-" +
@@ -27,6 +31,36 @@ let finDia =
     ("00" + 59).slice(-2);
 console.log(finDia);
 
+
+let url = "http://localhost:8084/resultados";
+fetch(url)
+    .then(response => response.json())
+    .then(dataevento => mostrarData(dataevento))
+    .catch(error => console.log(error))
+
+/*
+let date = new Date();
+
+let principioDia =
+    date.getFullYear() + "-" +
+    ("00" + (date.getMonth() + 1)).slice(-2) + "-" +
+    ("00" + date.getDate()).slice(-2) + " " +
+
+    ("00" + 00).slice(-2) + ":" +
+    ("00" + 00).slice(-2) + ":" +
+    ("00" + 00).slice(-2);
+console.log(principioDia);
+
+let finDia =
+    date.getFullYear() + "-" +
+    ("00" + (date.getMonth() + 1)).slice(-2) + "-" +
+    ("00" + date.getDate()).slice(-2) + " " +
+
+    ("00" + 23).slice(-2) + ":" +
+    ("00" + 59).slice(-2) + ":" +
+    ("00" + 59).slice(-2);
+console.log(finDia);
+*/
 
 const mostrarData = (dataevento) => {
     console.log(dataevento)
@@ -52,17 +86,18 @@ function fetchEventoFinalizado() {
 
     const mostrarData = (dataevento) => {
 
-
-        let date = new Date();
-        let fecha =
-            date.getFullYear() + "-" +
-            ("00" + (date.getMonth() + 1)).slice(-2) + "-" +
-            ("00" + date.getDate()).slice(-2) + " " +
-
-            ("00" + date.getHours()).slice(-2) + ":" +
-            ("00" + date.getMinutes()).slice(-2) + ":" +
-            ("00" + date.getSeconds()).slice(-2);
-        console.log(fecha);
+        /*
+                let date = new Date();
+                let fecha =
+                    date.getFullYear() + "-" +
+                    ("00" + (date.getMonth() + 1)).slice(-2) + "-" +
+                    ("00" + date.getDate()).slice(-2) + " " +
+        
+                    ("00" + date.getHours()).slice(-2) + ":" +
+                    ("00" + date.getMinutes()).slice(-2) + ":" +
+                    ("00" + date.getSeconds()).slice(-2);
+                console.log(fecha);
+        */
 
         dataevento.sort((a, b) => parseFloat(a.fechaHora) - parseFloat(b.fechaHora));
         dataevento.reverse();
@@ -91,6 +126,7 @@ function fetchEventoProximo() {
 
     const mostrarData = (dataevento) => {
 
+        /*
         let date = new Date();
         let fecha =
             date.getFullYear() + "-" +
@@ -101,7 +137,7 @@ function fetchEventoProximo() {
             ("00" + date.getMinutes()).slice(-2) + ":" +
             ("00" + date.getSeconds()).slice(-2);
         console.log(fecha);
-
+*/
 
         dataevento.sort((a, b) => parseFloat(a.fechaHora) - parseFloat(b.fechaHora));
         console.log(dataevento);
@@ -138,6 +174,20 @@ function basket() {
         .then(dataevento => mostrarData(dataevento))
         .catch(error => console.log(error))
 
+        let basket = document.getElementById('barraResultados');
+        let htmlCode = '<ul id="barraBasket"><li><a onclick="directo();" >Hoy</a></li><li><a onclick="finalizado(); fetchEventoFinalizadoBasket()">Finalizados</a></li><li><a onclick="proximo(); fetchEventoProximoBasket()">Proximos</a></li></ul>';
+        basket.insertAdjacentHTML('beforebegin', htmlCode);
+        document.getElementById('barraResultados').style.display = "none";
+        
+    
+        let barradeporte = document.getElementById('barraDeportes');
+        let htmlCodes = '<ul id=""><li><a href="">Futbol</a></li><li><a id="">Basketball</a></li><li><a href="">Tenis</a></li><li><a href="">Deporte</a></li><li><a href="">Deporte</a></li></ul>';
+        barradeporte.insertAdjacentHTML('beforebegin', htmlCodes);
+        document.getElementById('barraDeportes').style.display = "none";
+    
+
+        /*
+
     let date = new Date();
 
     let principioDia =
@@ -160,6 +210,7 @@ function basket() {
         ("00" + 59).slice(-2) + ":" +
         ("00" + 59).slice(-2);
     console.log(finDia);
+*/
 
     const mostrarData = (dataevento) => {
         console.log(dataevento)
@@ -175,5 +226,72 @@ function basket() {
             }
         }
         document.getElementById('dataevento').innerHTML = body;
+    }
+}
+
+
+
+
+function fetchEventoFinalizadoBasket() {
+
+    let url = "http://localhost:8084/resultados";
+    fetch(url)
+        .then(response => response.json())
+        .then(dataevento => mostrarData(dataevento))
+        .catch(error => console.log(error))
+
+
+    const mostrarData = (dataevento) => {
+
+        dataevento.sort((a, b) => parseFloat(a.fechaHora) - parseFloat(b.fechaHora));
+        dataevento.reverse();
+
+
+        console.log(dataevento);
+        let body = ''
+        for (let i = 0; i < dataevento.length; i++) {
+            if (dataevento[i].deporte == "basketball") {
+
+
+                if (dataevento[i].fechaHora <= fecha) {
+                    body += `<tr><td>${dataevento[i].fechaHora}</td><td>${dataevento[i].infracciones}</td><td>${dataevento[i].resultado}</td><td>${dataevento[i].ubicacion}</td></tr>`
+
+                }
+            }
+
+        }
+
+        document.getElementById('dataeventoFin').innerHTML = body;
+    }
+}
+
+function fetchEventoProximoBasket() {
+
+    let url = "http://localhost:8084/resultados";
+    fetch(url)
+        .then(response => response.json())
+        .then(dataevento => mostrarData(dataevento))
+        .catch(error => console.log(error))
+
+
+    const mostrarData = (dataevento) => {
+
+
+        dataevento.sort((a, b) => parseFloat(a.fechaHora) - parseFloat(b.fechaHora));
+        console.log(dataevento);
+
+
+        let body = ''
+        for (let i = 0; i < dataevento.length; i++) {
+            if (dataevento[i].deporte == "basketball") {
+
+                if (dataevento[i].fechaHora >= fecha) {
+
+                    body += `<tr><td>${dataevento[i].fechaHora}</td><td>${dataevento[i].infracciones}</td><td>${dataevento[i].resultado}</td><td>${dataevento[i].ubicacion}</td></tr>`
+                }
+            }
+
+        }
+        document.getElementById('dataeventoProx').innerHTML = body;
     }
 }
