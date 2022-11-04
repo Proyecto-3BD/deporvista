c3797f4aa59e1d1f890f38fd7a20ea90dd047419
veoDoble <gustavoguerrerom@gmail.com>
2022-11-04 01:59:20 -0300

    agrego vistas de backoffice

---
+++ backoffice/vistas/templates/sidebar.php
@@ -0,0 +1,8 @@
+<div class="sidebar">
+  <a href="/">Inicio</a> 
+  <a href="/suscriptores">Suscriptores</a>
+  <a href="/gestionAdministradores">Administradores</a>
+  <a href="/gestionBanners">Anuncios</a>
+  <a href="/gestionCompeticiones">Competiciones</a>
+  <a href="/gestionEventos">Eventos</a>
+</div>
