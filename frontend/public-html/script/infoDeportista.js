095e0a572891d0d263b3985d2b710a56ae533b25
francomacedo96 <francomacedo@outlook.com>
2022-11-05 18:37:59 -0300

    probando ligas

--- frontend/public-html/script/infoDeportista.js
+++ frontend/public-html/script/infoDeportista.js
@@ -162,9 +162,7 @@ function fetchEventoProximo() {
 }
 
 
-
-
-/* probando BASKET */
+/*  BASKET */
 
 let mibasket = document.getElementById("basket");
 let eventobasket = mibasket.addEventListener('click', basket);
@@ -292,6 +290,123 @@ function fetchEventoProximoBasket() {
         let body = ''
         for (let i = 0; i < dataevento.length; i++) {
             if (dataevento[i].deporte == "basketball") {
+
+                if (dataevento[i].fechaHora >= fecha) {
+
+                    body += `<tr><td>${dataevento[i].fechaHora}</td><td>${dataevento[i].locatario}</td><td>${dataevento[i].visitante}</td><td>${dataevento[i].ubicacion}</td></tr>`
+                }
+            }
+
+        }
+        document.getElementById('dataeventoProx').innerHTML = body;
+    }
+}
+
+
+/* De aca para abajo prueba */
+/* Liga */
+let miligaUru = document.getElementById("miligauru");
+let eventomiligaUru = miligaUru.addEventListener('click', ligauy);
+
+document.getElementById("general").style.display = "none";
+document.getElementById("porliga").style.display = "block";
+
+
+function ligauy() {
+    let url = "http://localhost:8084/resultados";
+    fetch(url)
+        .then(response => response.json())
+        .then(dataevento => mostrarData(dataevento))
+        .catch(error => console.log(error))
+
+    document.getElementById("hoy").style.display = "block";
+    document.getElementById("finalizados").style.display = "none";
+    document.getElementById("proximos").style.display = "none";
+
+
+    let basket = document.getElementById('barraResultadosLiga');
+    let htmlCode = '<ul><li><a onclick="directo();" >Hoy</a></li><li><a onclick="finalizado(); fetchEventoFinalizadoligauy()">Finalizados</a></li><li><a onclick="proximo(); fetchEventoProximoligauy()">Proximos</a></li></ul>';
+    basket.insertAdjacentHTML('beforebegin', htmlCode);
+    document.getElementById('barraResultadosLiga').style.display = "none";
+
+/*
+    let barradeporte = document.getElementById('barraDeportes');
+    let htmlCodes = '<ul id=""><li><a href="">Futbol</a></li><li><a id="">Basketball</a></li><li><a href="">Tenis</a></li><li><a href="">Rugby</a></li><li><a href="">Golf</a></li></ul>';
+    barradeporte.insertAdjacentHTML('beforebegin', htmlCodes);
+    document.getElementById('barraDeportes').style.display = "none";
+*/
+    const mostrarData = (dataevento) => {
+        console.log(dataevento)
+        let body = ''
+        for (let i = 0; i < dataevento.length; i++) {
+
+            if (dataevento[i].competiciones == "Primera division de Uruguay") {
+
+                if (dataevento[i].fechaHora > principioDia && dataevento[i].fechaHora < finDia) {
+
+                    body += `<tr><td>${dataevento[i].fechaHora}</td><td>${dataevento[i].locatario}</td><td>${dataevento[i].resultado}</td><td>${dataevento[i].visitante}</td><td>${dataevento[i].ubicacion}</td></tr>`
+                }
+            }
+        }
+        document.getElementById('dataevento').innerHTML = body;
+    }
+}
+
+
+
+
+function fetchEventoFinalizadoligauy() {
+
+    let url = "http://localhost:8084/resultados";
+    fetch(url)
+        .then(response => response.json())
+        .then(dataevento => mostrarData(dataevento))
+        .catch(error => console.log(error))
+
+
+    const mostrarData = (dataevento) => {
+
+        dataevento.sort((a, b) => parseFloat(a.fechaHora) - parseFloat(b.fechaHora));
+        dataevento.reverse();
+
+
+        console.log(dataevento);
+        let body = ''
+        for (let i = 0; i < dataevento.length; i++) {
+            if (dataevento[i].competiciones == "Primera division de Uruguay") {
+
+
+                if (dataevento[i].fechaHora <= fecha) {
+                    body += `<tr><td>${dataevento[i].fechaHora}</td><td>${dataevento[i].locatario}</td><td>${dataevento[i].resultado}</td><td>${dataevento[i].visitante}</td><td>${dataevento[i].infracciones}</td><td>${dataevento[i].ubicacion}</td></tr>`
+
+                }
+            }
+
+        }
+
+        document.getElementById('dataeventoFin').innerHTML = body;
+    }
+}
+
+function fetchEventoProximoligauy() {
+
+    let url = "http://localhost:8084/resultados";
+    fetch(url)
+        .then(response => response.json())
+        .then(dataevento => mostrarData(dataevento))
+        .catch(error => console.log(error))
+
+
+    const mostrarData = (dataevento) => {
+
+
+        dataevento.sort((a, b) => parseFloat(a.fechaHora) - parseFloat(b.fechaHora));
+        console.log(dataevento);
+
+
+        let body = ''
+        for (let i = 0; i < dataevento.length; i++) {
+            if (dataevento[i].competiciones == "Primera division de Uruguay") {
 
                 if (dataevento[i].fechaHora >= fecha) {
 
