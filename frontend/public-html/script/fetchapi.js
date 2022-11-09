4b3acc117906ceaf60201454e5f3d267ee5263f2
francomacedo96 <francomacedo@outlook.com>
2022-11-09 16:03:47 -0300

    alerta

--- frontend/public-html/script/fetchapi.js
+++ frontend/public-html/script/fetchapi.js
@@ -9,9 +9,9 @@ function fetchpost() {
       body: new FormData(formularioenvio)
     });
     let result = await response.json();
-    if(result.resultado === "true") {
+    /*if(result.resultado === "true") {
         alert("entro");
-    } 
+    } */
 };
 }
 
