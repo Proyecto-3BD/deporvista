66c5a4439e9368731d0e1fcd837914c4530af46d
veoDoble <gustavoguerrerom@gmail.com>
2022-11-10 02:39:07 -0300

    chau sidebar

--- backoffice/vistas/templates/sidebar.php
+++
@@ -1,8 +0,0 @@
-<div class="sidebar">
-  <a href="/">Inicio</a> 
-  <a href="/suscriptores">Suscriptores</a>
-  <a href="/gestionAdministradores">Administradores</a>
-  <a href="/gestionBanners">Anuncios</a>
-  <a href="/gestionCompeticiones">Competiciones</a>
-  <a href="/gestionEventos">Eventos</a>
-</div>
